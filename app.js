fab4805db6289c0607a22811aaccbc326ea12489
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-22 15:01:33 -0500

    changed to querySelectorAll

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const myList = document.getElementsByTagName('li');
+const myNonPurpleList = document.querySelectorAll('.error-not-purple');
 
 for (let i=0; i < myList.length; i +=1){
-    myList[i].style.color = 'purple'
+    myList[i].style.color = 'purple';
+}
+
+for (let i=0; i < myNonPurpleList.length; i +=1){
+    myNonPurpleList[i].style.color = 'red';
 }
