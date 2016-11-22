38155e0e4a57d766613639af0d3abf606077d8e8
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-22 13:55:58 -0500

    added button

--- app.js
+++ app.js
@@ -3,4 +3,9 @@
  */
 
 
-const myHeading = document.getELementById('myHeading');
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => {
+    myHeading.style.color = 'red';
+});
