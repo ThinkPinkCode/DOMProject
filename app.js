e9b959bd48570d975d8981642ed998e53ac3ff50
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-22 14:11:46 -0500

    added subheader color button

--- app.js
+++ app.js
@@ -4,8 +4,16 @@
 
 
 const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
+const subheading = document.getElementById('subheading');
+const myButton  = document.getElementById('myButton');
+const myButton2 = document.getElementById('myButton2');
+const myTextInput = document.getElementById('myTextInput');
+const myTextInput2 = document.getElementById('myTextInput2');
 
 myButton.addEventListener('click', () => {
-    myHeading.style.color = 'red';
+    myHeading.style.color = myTextInput.value;
 });
+
+myButton2.addEventListener('click', () => {
+    subheading.style.color = myTextInput2.value;
+})
