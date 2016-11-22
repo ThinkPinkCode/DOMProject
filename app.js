0721ff32672b93b4a4b3c79fdf4f820f94947d9b
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-22 15:30:22 -0500

    added nth child selector

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const myList = document.getElementsByTagName('li');
 const myNonPurpleList = document.querySelectorAll('.error-not-purple');
+const evens = document.querySelectorAll('li:nth-child(odd)');
 
 for (let i=0; i < myList.length; i +=1){
     myList[i].style.color = 'purple';
@@ -7,4 +8,8 @@ for (let i=0; i < myList.length; i +=1){
 
 for (let i=0; i < myNonPurpleList.length; i +=1){
     myNonPurpleList[i].style.color = 'red';
+}
+
+for (let i=0; i < evens.length; i +=1){
+    evens[i].style.backgroundColor = 'lightgray';
 }
