745048addf7f8ffec35438bf36b5abc8b2d35194
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-22 14:36:15 -0500

    add purple list

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-/**
- * Created by melanie.myers on 11/17/16.
- */
+const myList = document.getElementsByTagName('li');
 
-
-const myHeading = document.getElementsByTagName('h1')[0];
-const myButton  = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
-
-
-myButton.addEventListener('click', () => {
-    myHeading.style.color = myTextInput.value;
-});
-
+for (let i=0; i < myList.length; i +=1){
+    myList[i].style.color = 'purple'
+}
