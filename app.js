700392c16290f1a6919adde881e497ad586d64c8
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-23 10:12:48 -0500

    content change button

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const myList = document.getElementsByTagName('li');
-const myNonPurpleList = document.querySelectorAll('.error-not-purple');
-const evens = document.querySelectorAll('li:nth-child(odd)');
+const input = document.querySelector('input');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button');
 
-for (let i=0; i < myList.length; i +=1){
-    myList[i].style.color = 'purple';
-}
-
-for (let i=0; i < myNonPurpleList.length; i +=1){
-    myNonPurpleList[i].style.color = 'red';
-}
-
-for (let i=0; i < evens.length; i +=1){
-    evens[i].style.backgroundColor = 'lightgray';
-}
+button.addEventListener('click', () => {
+    p.textContent = input.value + ':';
+})
